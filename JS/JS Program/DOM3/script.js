e968dffbe903cd5b61c03c3c0fe38da249f2b3a5
Prabhanjan Amanta <pravanjan2003p@gmail.com>
2024-12-26 00:32:28 +0530

    update

--- JS/JS Program/DOM3/script.js
+++ JS/JS Program/DOM3/script.js
@@ -1,3 +1,28 @@
+// var mainDiv = document.getElementById('main')
+// var c = 0;
+// // function addData ()
+// // {
+// // c++;
+// //     var newPara = document.createElement('p')//<p></p>
+// //     newPara.textContent = "Prasad"+c;//<p>prasad</p>
+// //     mainDiv.appendChild(newPara)
+// // }
+
+// var mname = document.getElementById('mname');
+// var murl = document.getElementById('url');
+// var getMovie = function ()
+// {
+//     var mdiv = document.createElement('div');
+//     var mimg = document.createElement('img');
+//     mimg.src = murl.value;
+//     mimg.alt = mname.value;
+//     var mtitle = document.createElement('h2')
+//     mtitle.textContent = mname.value;
+//     mdiv.append(mimg, mtitle)
+//     mainDiv.appendChild(mdiv)
+// }
+
+
 var mainDiv = document.getElementById('main')
 var c = 0;
 // function addData ()
@@ -10,14 +35,30 @@ var c = 0;
 
 var mname = document.getElementById('mname');
 var murl = document.getElementById('url');
-var getMovie = function ()
+// var getMovie = function ()
+// {
+//     var mdiv = document.createElement('div');
+//     var mimg = document.createElement('img');
+//     mimg.src = murl.value;
+//     mimg.alt = mname.value;
+//     var mtitle = document.createElement('h2')
+//     mtitle.textContent = mname.value;
+//     mdiv.append(mimg, mtitle)
+//     mainDiv.appendChild(mdiv)
+// }
+
+var changeColor = function (event)
 {
-    var mdiv = document.createElement('div');
-    var mimg = document.createElement('img');
-    mimg.src = murl.value;
-    mimg.alt = mname.value;
-    var mtitle = document.createElement('h2')
-    mtitle.textContent = mname.value;
-    mdiv.append(mimg, mtitle)
-    mainDiv.appendChild(mdiv)
+    console.log(event.key)
+    // if (event.key == 'Enter')
+    // {
+      const rdcolor = "#" + Math.round(Math.random() * 10000000).toString(16)
+			document.body.style.backgroundColor = rdcolor  
+    // }
+    
 }
+document.addEventListener('scroll', function (e)
+{
+  console.log(e)
+})
+document.body.addEventListener("keydown", changeColor)
---
+++ JS/Projects/project-5 clock/script.js
@@ -0,0 +1,156 @@
+// const min = document.getElementById('min');
+// const sec = document.getElementById('sec');
+// let secCount = 60;
+// let minCount = 5;
+
+// function countdown() {
+//     secCount--;
+//     if(secCount <=0){
+//         secCount = 59;
+//         minCount--;
+//     }
+//     min.textContent = minCount;     
+//     sec.textContent = secCount;
+//     console.log(secCount);
+// }
+// function stopTime(){
+//     clearInterval(countdown);
+//     console.log("Time's up!");
+//     min.textContent = "00";
+//     sec.textContent = "00";
+// }
+// setInterval(countdown, 1000)
+// setTimeout(stopTime,5000);
+
+
+
+
+const min = document.getElementById('min');
+const sec = document.getElementById('sec');
+let secCount;
+let minCount;
+let intervalId = null;
+
+function countdown() {
+    // minCount = 1;
+    secCount--;
+    if (secCount < 0) {
+        secCount = 59;
+        minCount--;
+    }
+    if (minCount < 0) {
+        stopTime();
+        return;
+    }
+    min.textContent = minCount.toString().padStart(2, '0');
+    sec.textContent = secCount.toString().padStart(2, '0');
+}
+
+function stopTime() {
+    clearInterval(intervalId);
+    // console.log("Time's up!");
+    min.textContent = "00";
+    sec.textContent = "00";
+}
+
+function startTimer() {
+    if (!intervalId) {
+        intervalId = setInterval(countdown, 1000);
+    }
+}
+
+function pauseTimer() {
+    clearInterval(intervalId);
+    intervalId = null;
+}
+
+function resetTimer() {
+    clearInterval(intervalId);
+    intervalId = null;
+    secCount = 0;
+    minCount = 0;
+    min.textContent = "00";
+    sec.textContent = "00";
+}
+
+// Event listeners for the buttons
+document.querySelector(".box3 button:nth-child(1)").addEventListener('click', pauseTimer); // Pause
+document.querySelector(".box3 button:nth-child(2)").addEventListener('click', startTimer); // Start
+document.querySelector(".box3 button:nth-child(3)").addEventListener('click', resetTimer); // Reset
+
+
+//5min 
+function start5() {
+    minCount = 4;
+    secCount = 60;
+    countdown();
+    if (!intervalId) {
+        intervalId = setInterval(countdown, 1000);
+    }
+}
+//10min 
+function start10() {
+    minCount = 9;
+    secCount = 60;
+    countdown();
+    if (!intervalId) {
+        intervalId = setInterval(countdown, 1000);
+    }
+}
+//30min 
+function start30() {
+    minCount = 29;
+    secCount = 60;
+    countdown();
+    if (!intervalId) {
+        intervalId = setInterval(countdown, 1000);
+    }
+}
+//Custome
+// cstm open
+const cstm = document.getElementById("custm");
+const popCstm = document.querySelector(".popCustm");
+var chc = 1
+function openCstm() {
+    const colorCstm4 = document.getElementById("colorCstm4");
+    const colorCstm3 = document.getElementById("colorCstm3");
+    const colorCstm2 = document.getElementById("colorCstm2");
+    const colorCstm1 = document.getElementById("colorCstm1");
+    if (chc) {
+        chc = 0;
+        colorCstm4.style.color ='black';
+        colorCstm3.style.color ='rgb(164, 161, 161)';
+        colorCstm2.style.color ='rgb(164, 135, 135)';
+        colorCstm1.style.color ='rgb(164 , 135, 135)';
+        popCstm.style.left = '420px';
+    } else {
+        colorCstm4.style.color ='#686663';
+        colorCstm3.style.color ='#686663';
+        colorCstm2.style.color ='#686663';
+        colorCstm1.style.color ='#686663';
+        popCstm.style.left = '-620px';
+        chc = 1;
+    }
+}
+
+
+//
+
+const popCstm1 = document.querySelector(".popCustm");
+const addTime = document.getElementById("qwe");
+var nwTime=0
+function addTime2() {
+
+    const cstmMin = document.getElementById("cstmMin").value;
+    nwTime = parseInt(cstmMin) ;
+    console.log(nwTime)
+    popCstm1.style.left = '-620px';
+    document.getElementById("cstmMin").textContent= "0";
+
+    minCount = nwTime-1;
+    secCount = 60;
+    countdown();
+    if (!intervalId) {
+        intervalId = setInterval(countdown, 1000);
+    }
+}

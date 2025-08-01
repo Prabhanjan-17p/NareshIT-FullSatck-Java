19574480e4b3cd74101436cc92e4ff5feafdac2e
Prabhanjan Amanta <pravanjan2003p@gmail.com>
2025-08-02 00:08:43 +0530

    update

---
+++ Java Frameworks/Note/Spring Boot Core/Spring Boot Core Class App/BootIocProj-04_Strategy_Design_Pattern_Imprvies_FactoryBeanMethod/src/main/java/com/nt/sbean/Vehicle.java
@@ -0,0 +1,19 @@
+package com.nt.sbean;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component("vehicle")
+public class Vehicle{
+	
+	@Autowired
+	private IEngine engine;
+	
+	public void journey(String st,String end) {
+		System.out.println("Journey Started :: "+st);
+		engine.startEngine();
+		System.out.println("Journey going on!!!");
+		engine.stopEngine();
+		System.out.println("Journey End :: "+end);
+	}
+}

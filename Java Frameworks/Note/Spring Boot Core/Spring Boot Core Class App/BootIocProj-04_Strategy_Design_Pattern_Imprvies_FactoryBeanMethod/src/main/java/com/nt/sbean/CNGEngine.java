50996fd4b609175bf27f82a1237a4d6df3dc8e34
Prabhanjan Amanta <pravanjan2003p@gmail.com>
2025-08-02 00:11:37 +0530

    Create CNGEngine.java

---
+++ Java Frameworks/Note/Spring Boot Core/Spring Boot Core Class App/BootIocProj-04_Strategy_Design_Pattern_Imprvies_FactoryBeanMethod/src/main/java/com/nt/sbean/CNGEngine.java
@@ -0,0 +1,15 @@
+package com.nt.sbean;
+
+public class CNGEngine implements IEngine {
+
+	@Override
+	public void startEngine() {
+		System.out.println("CNGEngine Started!!!");
+	}
+
+	@Override
+	public void stopEngine() {
+		System.out.println("CNGEngine Stop!!!");
+	}
+
+}

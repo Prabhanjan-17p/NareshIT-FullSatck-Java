c4c2ede770e8e8041bd9e7b9cd019fcce45b7f5d
Prabhanjan Amanta <pravanjan2003p@gmail.com>
2025-08-02 00:09:47 +0530

    update

--- Java Frameworks/Note/Spring Boot Core/Spring Boot Core Class App/BootIocProj-03_Strategy_Design_Pattern_Imprvies_XML/src/main/java/com/nt/BootProj03StrategyDesignPatternApplicationImproveXML.java
+++ Java Frameworks/Note/Spring Boot Core/Spring Boot Core Class App/BootIocProj-03_Strategy_Design_Pattern_Imprvies_XML/src/main/java/com/nt/BootProj03StrategyDesignPatternApplicationImproveXML.java
@@ -3,14 +3,16 @@ package com.nt;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ImportResource;
 
 import com.nt.sbean.Vehicle;
 
 @SpringBootApplication
-public class BootProj02StrategyDesignPatternApplication {
+@ImportResource("com/nt/config/applicationContext.xml")
+public class BootProj03StrategyDesignPatternApplicationImproveXML {
 
 	public static void main(String[] args) {
-		try(ConfigurableApplicationContext context = SpringApplication.run(BootProj02StrategyDesignPatternApplication.class, args);) {
+		try(ConfigurableApplicationContext context = SpringApplication.run(BootProj03StrategyDesignPatternApplicationImproveXML.class, args);) {
 			Vehicle vehicle = context.getBean(Vehicle.class);
 			vehicle.journey("Odish", "Hyderabad");
 		} catch (Exception e) {

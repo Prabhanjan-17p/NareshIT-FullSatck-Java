4e76bf0fe560369a0a941815c4664d5ac3e6f20b
Prabhanjan Amanta <pravanjan2003p@gmail.com>
2025-08-31 23:51:57 +0530

    Update

---
+++ DevSecOps Tools/ELK Stack/Class Room App/ELK_Proj01/src/main/java/com/example/demo/ElkProj01Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElkProj01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElkProj01Application.class, args);
+	}
+
+}

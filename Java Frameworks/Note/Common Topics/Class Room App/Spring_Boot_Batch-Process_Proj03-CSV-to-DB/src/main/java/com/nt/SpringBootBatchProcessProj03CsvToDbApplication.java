5faa86a920c6e798cd98c0208c873c57e9d589df
Prabhanjan Amanta <pravanjan2003p@gmail.com>
2025-09-09 00:30:27 +0530

    update spring batch processing file and code

---
+++ Java Frameworks/Note/Common Topics/Class Room App/Spring_Boot_Batch-Process_Proj03-CSV-to-DB/src/main/java/com/nt/SpringBootBatchProcessProj03CsvToDbApplication.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBatchProcessProj03CsvToDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBatchProcessProj03CsvToDbApplication.class, args);
+	}
+
+}

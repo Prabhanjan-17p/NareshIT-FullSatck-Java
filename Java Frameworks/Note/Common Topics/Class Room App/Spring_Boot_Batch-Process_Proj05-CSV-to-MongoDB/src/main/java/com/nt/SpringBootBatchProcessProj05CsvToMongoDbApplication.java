da5468613ddbaec0b2a2ba1395b53de464dfac11
Prabhanjan Amanta <pravanjan2003p@gmail.com>
2025-09-14 01:12:18 +0530

    Spring Data Batch Process

---
+++ Java Frameworks/Note/Common Topics/Class Room App/Spring_Boot_Batch-Process_Proj05-CSV-to-MongoDB/src/main/java/com/nt/SpringBootBatchProcessProj05CsvToMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBatchProcessProj05CsvToMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBatchProcessProj05CsvToMongoDbApplication.class, args);
+	}
+
+}

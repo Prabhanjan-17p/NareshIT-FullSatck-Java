b8aad33fdf691649781890776afa6b7c0c1db32b
Prabhanjan Amanta <pravanjan2003p@gmail.com>
2025-09-18 00:10:29 +0530

    Query Method Update

---
+++ Java Frameworks/Note/Spring Boot Data JPA/Spring Boot Data JPA Class Room App/Spring-Data-JPA-Proj05_Custom-Method_Query-Method/src/main/java/com/nt/SpringDataJpaProj05CustomMethodQueryMethodApplication.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaProj05CustomMethodQueryMethodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaProj05CustomMethodQueryMethodApplication.class, args);
+	}
+
+}

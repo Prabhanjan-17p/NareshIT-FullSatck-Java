53fdc3e67653652079b04205abbe24baeeee4ebd
Prabhanjan Amanta <pravanjan2003p@gmail.com>
2025-09-26 03:41:30 +0530

    Update Data JPA

---
+++ Java Frameworks/Note/Spring Boot Data JPA/Spring Boot Data JPA Class Room App/Spring-Data-JPA-Proj10_WorkingWith-LOB-CLOB-Boolean/src/main/java/com/nt/SpringDataJpaProj10WorkingWithLobClobBooleanApplication.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaProj10WorkingWithLobClobBooleanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaProj10WorkingWithLobClobBooleanApplication.class, args);
+	}
+
+}
